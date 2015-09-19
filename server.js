25de8539e1f1b955afabafed33aca12b8f6ec935
Jason J. Gullickson <jason.gullickson@gmail.com>
2015-09-19 11:36:24 -0500

    Endpoint handlers stubbed, primary endpoints defined

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ server.on('MethodNotAllowed', unknownMethodHandler);
 // endpoint handlers
 function get_status(req, res, next){
 	log.message(log.DEBUG, "get_status()");
-	res.send("all good");
+	res.send(200);
 	return next;
 }
 
@@ -44,16 +44,80 @@ function new_bot(req, res, next){
 }
 
 // TODO: get bot
+function get_bot(req, res, next){
+	log.message(log.DEBUG, "get_bot()");
+	res.send(200);
+	return next;
+}
+
+// TODO: update bot
+function update_bot(req, res, next){
+	log.message(log.DEBUG, "update_bot");
+	res.send(200);
+	return next;
+}
+
 // TODO: delete bot
+function delete_bot(req, res, next){
+	log.message(log.DEBUG, "delete_bot()");
+	res.send(200);
+	return next;
+}
+
 // TODO: list bots
+function list_bots(req, res, next){
+	log.message(log.DEBUG, "list_bots");
+	res.send(200);
+	return next;
+}
+
 // TODO: create a new message
+function new_message(req, res, next){
+	log.message(log.DEBUG, "new_message()");
+	res.send(200);
+	return next;
+}
+
 // TODO: get message
+function get_message(req, res, next){
+	log.message(log.DEBUG, "get_message()");
+	res.send(200);
+	return next;
+}
+
+// TODO: update message
+function update_message(req, res, next){
+	log.message(log.DEBUG, "update_message()");
+	res.send(200);
+	return next;
+}
+
 // TODO: delete message
+function delete_message(req, res, next){
+	log.message(log.DEBUG, "delete_message()");
+	res.send(200);
+	return next;
+}
+
 // TODO: list messages
+function list_messages(req, res, next){
+	log.message(log.DEBUG, "list_messages()");
+	res.send(200);
+	return next;
+}
 
 // endpoints
 server.get({path:"/status", version: "1.0.0"}, get_status);
+server.get({path:"/bots", version: "1.0.0"}, list_bots);
 server.post({path:"/bots", version: "1.0.0"}, new_bot);
+server.get({path:"/bots/:bot_name", version: "1.0.0"}, get_bot);
+server.put({path:"/bots/:bot_name", version: "1.0.0"}, update_bot);
+server.del({path:"/bots/:bot_name", version: "1.0.0"}, delete_bot);
+server.get({path:"/messages", version: "1.0.0"}, list_messages);
+server.post({path:"/messages", version: "1.0.0"}, new_message);
+server.get({path:"/messages/:message_id", version: "1.0.0"}, get_message);
+server.put({path:"/messages/:message_id", version: "1.0.0"}, update_message);
+server.del({path:"/messages/:message_id", version: "1.0.0"}, delete_message);
 
 // start server
 server.listen(config.SERVER_PORT, function() {
