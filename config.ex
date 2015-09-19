1e644827fbba0b7e6e2ac430e6421df1b6b8ea2c
Jason J. Gullickson <jason.gullickson@gmail.com>
2015-09-19 10:50:31 -0500

    added redis config and restify init

--- config.ex
+++ config.ex
@@ -1,4 +1,5 @@
 module.exports = {
 	LOG_LEVEL: 0,
-	SERVER_PORT: 5000
+	SERVER_PORT: 5000,
+	REDIS_URL:"redis://localhost:6379"
 };
--- server.js
+++ server.js
@@ -1,3 +1,7 @@
 var config = require("./config.js");
+var redis = require('redis-url').connect(config.REDIS_URL);
+var restify = require('restify');
 var log = require("jlog.js");
 log.level = config.LOG_LEVEL;
+
+
