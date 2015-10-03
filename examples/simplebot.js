42f209e296e4bb28808c79df0887f3078ee26322
Jason J. Gullickson <jason.gullickson@gmail.com>
2015-10-03 17:57:54 -0500

    functioning example bot

--- examples/simplebot.js
+++ examples/simplebot.js
@@ -27,20 +27,71 @@ register_req = http.request(register_options, function(register_res){
     console.log("New bot data recieved: " + buffer);
     new_bot = JSON.parse(buffer);
 
-    // TODO: post a message
+    // post a message
+    var first_message = {contents:"Greetings robot bretheren!"};
+    var message_options = {
+      hostname: "localhost",
+      port: 5000,
+      path: "/bots/" + new_bot.name + "/messages?token=" + new_bot.token,
+      method: "POST",
+      headers: {
+        "Content-Type":"application/json"
+      }
+    };
+    var message_req = http.request(message_options, function(message_res){
+      console.log("First message request status: " + message_res.statusCode);
+      var new_message_buffer = "";
+      message_res.on("data", function(chunk){
+        new_message_buffer+=chunk;
+      });
+      message_res.on("end", function(){
+        var message_result = JSON.parse(new_message_buffer);
+        new_bot.token = message_result.token;
 
-    // listen to firehose
-    console.log("Connecting to firehose");
-    var ws = new WebSocket("ws://localhost:8080");
-    ws.on("open", function open(){
-      console.log("Connected to firehose");
+        // listen to firehose
+        console.log("Connecting to firehose");
+        var ws = new WebSocket("ws://localhost:8080");
+        ws.on("open", function open(){
+          console.log("Connected to firehose");
+        });
+
+        ws.on("message", function(data, flags){
+          console.log("New message from firehose: " + data);
+          var new_firehose_message = JSON.parse(data);
+
+          // say hi to everyone that posts a message (except ourselves or replies)
+          if(new_firehose_message.source != new_bot.name && !new_firehose_message.reply_to){
+              var reply_message = {contents:"Hi " + new_firehose_message.source + "!!!",
+                                    reply_to:new_firehose_message.message_id};
+              var reply_options = {
+                hostname: "localhost",
+                port: 5000,
+                path: "/bots/" + new_bot.name + "/messages?token=" + new_bot.token,
+                method: "POST",
+                headers: {
+                  "Content-Type":"application/json"
+                }
+              };
+              var reply_req = http.request(reply_options, function(reply_res){
+                console.log("Reply message request status: " + reply_res.statusCode);
+                var reply_buffer = "";
+                reply_res.on("data", function(chunk){
+                  reply_buffer+=chunk;
+                });
+                reply_res.on("end", function(){
+                  var reply_result = JSON.parse(reply_buffer);
+                  new_bot.token = reply_result.token;
+                });
+              });
+              reply_req.write(JSON.stringify(reply_message));
+              reply_req.end();
+          } 
+        });
+
+      });
     });
-
-    ws.on("message", function(data, flags){
-      console.log("New message: " + data);
-
-      // TODO: when my name is mentioned, reply
-    });
+    message_req.write(JSON.stringify(first_message));
+    message_req.end();
   });
 });
 
